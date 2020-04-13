39befaa3b400bfc8b89b1e68aed1c2c09dce8ddd
sufyandaredevil <sufyandaredevil@gmail.com>
2020-04-13 18:36:43 +0530

    "separated concerns for scripts and register page validation improved"

---
+++ public/js/register.js
@@ -0,0 +1,27 @@
+function showParFields() {
+
+    const sel = document.getElementById('type').value;
+    if (sel === 'teacher') {
+      document.getElementById("myForm").reset();
+      document.getElementById('type').value = sel;
+      document.getElementById("rollNumber").required = false;
+      document.getElementById("department").required = false;
+      document.getElementById("yearOfJoining").required = false;
+      document.getElementById("year").required = false;
+      document.getElementById("student").style.display = "none";
+
+      document.getElementById("facultyid").required = true;
+      document.getElementById("teacher").style.display = "block";
+    } else if (sel === 'student') {
+      document.getElementById("myForm").reset();
+      document.getElementById('type').value = sel;
+      document.getElementById("facultyid").required = false;
+      document.getElementById("teacher").style.display = "none";
+
+      document.getElementById("rollNumber").required = true;
+      document.getElementById("department").required = true;
+      document.getElementById("yearOfJoining").required = true;
+      document.getElementById("year").required = true;
+      document.getElementById("student").style.display = "block";
+    }
+  }
--- routes/register.js
+++ routes/register.js
@@ -12,59 +12,78 @@ module.exports = (app, md5, RefconStudent, RefconTeacher) => {
     });
 
     app.post('/register', (req, res) => {
-        if(!req.session.user){
-        if(req.body.type === 'student'){
-            RefconStudent.findOne({$or: [{email: (req.body.email)}, {rollNumber: (req.body.rollNumber)}]}, (err, data) => {
-                if(err){
-                    console.log(err);
-                }
-                else{
-                    if(data){
-                        res.render("register", { typerror: "Email ID / Registration number already exists" });
+        if(!req.session.user){console.log(req.body);
+            
+            if(req.body.type ===  undefined){
+                res.render("register", { typerror: "Select which type you belong to" });
+            }
+            else if(req.body.type === 'student' && req.body.department === undefined){
+                res.render("register", { typerror: "Select your department" });
+            }
+            else if(req.body.type === 'student'){
+
+                const depCodes = {
+                    CIVIL: "103",
+                    CSE: "104",
+                    ECE: "106",
+                    EEE: "105",
+                    IT: "205",
+                    MECH: "114"
+                };
+
+                const genRollNumber = "1101"+req.body.yearOfJoining.substring(2)+depCodes[req.body.department]+req.body.rollNumber;
+
+                RefconStudent.findOne({$or: [{email: (req.body.email)}, {rollNumber: (req.body.rollNumber)}]}, (err, data) => {
+                    if(err){
+                        console.log(err);
                     }
                     else{
-                        const newRefconStudent = new RefconStudent({
-                            fullName: req.body.fullName,
-                            phoneNumber: req.body.phoneNumber,
-                            collegeName: req.body.collegeName,
-                            rollNumber: req.body.rollNumber,
-                            email: req.body.email,
-                            password: md5(req.body.password),
-                            type: req.body.type,
-                            department: (req.body.department).toUpperCase(),
-                            year: req.body.year
-                        });
-                        newRefconStudent.save();
-                        res.render("login", { typerror: "", success: "User Account created Successfully!"});   
-                    }            
-                }
-            });
-        }
-        else if(req.body.type === 'teacher'){
-            RefconTeacher.findOne({$or: [{email: (req.body.email)}, {facultyid: (req.body.facultyid)}]}, (err, data) => {
-                if(err){
-                    console.log(err);
-                }
-                else{
-                    if(data){
-                        res.render("register", { typerror: "Email ID / Faculty ID already exists" });
+                        if(data){
+                            res.render("register", { typerror: "Email ID / Registration number already exists" });
+                        }
+                        else{
+                            const newRefconStudent = new RefconStudent({
+                                fullName: req.body.fullName,
+                                phoneNumber: req.body.phoneNumber,
+                                collegeName: req.body.collegeName,
+                                rollNumber: genRollNumber,
+                                email: req.body.email,
+                                password: md5(req.body.password),
+                                type: req.body.type,
+                                department: (req.body.department).toUpperCase(),
+                                year: req.body.year
+                            });
+                            newRefconStudent.save();
+                            res.render("login", { typerror: "", success: "User Account created Successfully!"});   
+                        }            
+                    }
+                });
+            }
+            else if(req.body.type === 'teacher'){
+                RefconTeacher.findOne({$or: [{email: (req.body.email)}, {facultyid: (req.body.facultyid)}]}, (err, data) => {
+                    if(err){
+                        console.log(err);
                     }
                     else{
-                        const newRefconTeacher = new RefconTeacher({
-                            fullName: req.body.fullName,
-                            phoneNumber: req.body.phoneNumber,
-                            collegeName: req.body.collegeName,
-                            facultyid: req.body.facultyid,
-                            email: req.body.email,
-                            password: md5(req.body.password),
-                            type: req.body.type
-                        });
-                        newRefconTeacher.save();
-                        res.render("login", { typerror: "", success: "User Account created Successfully!"});   
-                    }            
-                }
-            });
-        }
+                        if(data){
+                            res.render("register", { typerror: "Email ID / Faculty ID already exists" });
+                        }
+                        else{
+                            const newRefconTeacher = new RefconTeacher({
+                                fullName: req.body.fullName,
+                                phoneNumber: req.body.phoneNumber,
+                                collegeName: req.body.collegeName,
+                                facultyid: req.body.facultyid,
+                                email: req.body.email,
+                                password: md5(req.body.password),
+                                type: req.body.type
+                            });
+                            newRefconTeacher.save();
+                            res.render("login", { typerror: "", success: "User Account created Successfully!"});   
+                        }            
+                    }
+                });
+            }
         }
     });
     
